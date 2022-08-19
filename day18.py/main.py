e9840d9c14304acd57538115f3422fa2a8734638
kanterohan <104501187+kanterohan@users.noreply.github.com>
2022-08-19 07:55:50 +0530

    Add files via upload

---
+++ day18.py/main.py
@@ -0,0 +1,41 @@
+# from ossaudiodev import SNDCTL_DSP_SETFRAGMENT
+# from turtle import _Screen, Turtle,screen
+
+# timmy_the_turtle=Turtle()
+
+# Screen=screen()
+# screen
+
+
+######## Challenge 1 - Draw a Square ############
+
+# import turtle as t
+
+
+# timmy_the_turtle = t.Turtle()
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(90)
+
+
+
+import turtle as t
+
+tim=t.Turtle()
+
+
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup
+    tim.forward(10)
+    tim.pendown
+
+
+
+
+
+
+
+
+
